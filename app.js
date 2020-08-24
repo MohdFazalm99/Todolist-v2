ca31ef9a82823e6adbd43205ba10da0416de8734
MohdFazalm99 <mohammadfazalm@gmail.com.com>
2020-08-24 11:09:16 +0530

    Add .gitignore file and Procfile. Database has been setup to the mongoose server

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect('mongodb://localhost:27017/todolistDb', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('mongodb+srv://admin-fazal:Fazal123@cluster0.obj1s.mongodb.net/todolistDb', {useNewUrlParser: true, useUnifiedTopology: true});
 
 const itemsSchema={
   name:String
@@ -138,6 +138,11 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function() {
+  console.log("Server has started successfully");
 });
